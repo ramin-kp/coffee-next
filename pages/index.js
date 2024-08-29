e28a136d5da5b514020a20dfc4e273e1ed690254
Ramin Karimpor <ramin.kp81@gmail.com>
2024-08-29 19:47:22 +0330

    develoing Services && About component

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,15 @@ import React from "react";
 
 //components
 import Slider from "@/components/template/Index/Slider";
+import Services from "@/components/template/Index/Services";
+import About from "@/components/template/Index/About";
 
 function Home() {
   return (
     <>
       <Slider />
+      <About />
+      <Services />
     </>
   );
 }
