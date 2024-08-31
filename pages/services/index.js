e1d921d0db1f4f7be01ab7e7046c3f669eed4d60
ramin-kp <ramin.kp81@gmail.com>
2024-08-31 22:26:08 +0330

    developing ServicesPage

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,7 +1,29 @@
 import React from "react";
 
-function Service() {
-  return <div>Service page</div>;
+//components
+import PageHeader from "@/components/module/PageHeader/PageHeader";
+import ServicesDetails from "@/components/template/Services/ServicesDetails";
+
+function Services({ data }) {
+  return (
+    <>
+      <PageHeader title={"Services"} href={"service"} />
+      <ServicesDetails data={data.services} />
+    </>
+  );
 }
 
-export default Service;
+export async function getStaticProps() {
+  const servicesResponse = await fetch("http://localhost:4000/services");
+  const servicesData = await servicesResponse.json();
+
+  return {
+    props: {
+      data: {
+        services: servicesData,
+      },
+    },
+  };
+}
+
+export default Services;
